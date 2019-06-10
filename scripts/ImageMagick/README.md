c9a66f4db53f73f5f94487ea4ed91e57c25f055b
Dr Nic Williams <drnicwilliams@gmail.com>
2019-06-10 09:56:47 +0930

    initial script to build ImageMagick

---
+++ scripts/ImageMagick/README.md
@@ -0,0 +1,58 @@
+# Build imagemagick within cflinuxfs3
+
+Our buildpack users will need to download a pre-compiled version of [imagemagick](https://www.imagemagick.org/). This Dockerfile describes how to compile imagemagick and output it as a `tgz` file. Another part of the toolchain will upload the tgz to a public place, from which buildpack users will download it on demand.
+
+```plain
+VERSION=7.0.8-49
+mkdir -p tmp/imagemagick-src
+curl -L -o tmp/imagemagick-src/imagemagick-$VERSION.tar.gz https://github.com/ImageMagick/ImageMagick/archive/$VERSION.tar.gz
+
+rm -rf tmp/imagemagick-output
+mkdir -p tmp/imagemagick-output
+docker run -ti \
+  -v $PWD:/buildpack \
+  -v $PWD/tmp/imagemagick-src:/imagemagick-src \
+  -v $PWD/tmp/imagemagick-output:/imagemagick-output \
+  -e "VERSION_REGEX=ImageMagick-(.*).tar.gz" \
+  -e SRC_DIR=/imagemagick-src \
+  -e OUTPUT_BLOBS_DIR=/imagemagick-output/blobs \
+  -e REPO_OUT=/imagemagick-output/pushme \
+  -e "GIT_NAME=$(git config user.name)" \
+  -e "GIT_EMAIL=$(git config user.email)" \
+  cloudfoundry/cflinuxfs3 \
+  /buildpack/scripts/ImageMagick/compile.sh
+```
+
+Then upload new compiled blobs to your S3 account/bucket. In CI, this is performed with an `s3` resource.
+
+```plain
+bucket=imagemagick-buildpack
+aws s3 cp --recursive tmp/imagemagick-output/blobs s3://$bucket/blobs/imagemagick
+```
+
+The updated `manifest.yml` is already committed into a cloned repo within `tmp/imagemagick-output/pushme`.
+
+```plain
+cat tmp/imagemagick-output/pushme/manifest.yml
+---
+language: ImageMagick
+default_versions:
+- name: ImageMagick
+  version: 7.0.8-49
+dependency_deprecation_dates: []
+
+include_files:
+  - README.md
+  - VERSION
+  - bin/supply
+  - manifest.yml
+pre_package: scripts/build.sh
+
+dependencies:
+- name: ImageMagick
+  version: 7.0.8-49
+  uri: https://ImageMagick-buildpack.s3.us-east-2.amazonaws.com/blobs/ImageMagick/ImageMagick-compiled-7.0.8-49.tgz
+  sha256: 0ee2bb83eefa717142d56b38b85ca5815bcf8fdef1ad1b078331a65c629c5588
+  cf_stacks:
+  - cflinuxfs3
+```
---
+++ scripts/ImageMagick/compile.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+set -eu
+
+LIBRARY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LIBRARY=${LIBRARY:-$(basename $LIBRARY_DIR)}
+echo "Compiling $LIBRARY"
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+: ${SRC_DIR:?required}
+: ${OUTPUT_BLOBS_DIR:?required}
+: ${REPO_OUT:?required}
+: ${GIT_EMAIL:?required}
+: ${GIT_NAME:?required}
+REPO_BRANCH=${REPO_BRANCH:-master}
+
+SRC_DIR=$PWD/${SRC_DIR}
+OUTPUT_BLOBS_DIR=$PWD/$OUTPUT_BLOBS_DIR
+
+# either set $VERSION, or pass in from ${VERSION_FROM} file (e.g. concourse resource)
+VERSION=${VERSION:-}
+if [[ ${VERSION_REGEX:-X} != "X" ]]; then
+  pushd $SRC_DIR
+  f=$(ls $LIBRARY*)
+  [[ $f =~ $VERSION_REGEX ]] && { VERSION="${BASH_REMATCH[1]}"; }
+  popd
+fi
+VERSION=${VERSION:-$(cat ${VERSION_FROM})}
+echo "VERSION: $VERSION"
+
+S3_BUCKET=${S3_BUCKET:-$LIBRARY-buildpack}
+S3_REGION=${S3_REGION:-us-east-2}
+
+TMP_SRC_DIR=${TMP_DIR:-tmp/src}
+TMP_SRC_DIR=$PWD/${TMP_SRC_DIR}
+TMP_BUILD_DIR=${TMP_DIR:-tmp/build}
+TMP_BUILD_DIR=$PWD/${TMP_BUILD_DIR}
+mkdir -p $TMP_SRC_DIR
+pushd $TMP_SRC_DIR
+rm -rf $LIBRARY-*/
+
+SRC_ARCHIVE=$(ls $SRC_DIR/$LIBRARY*)
+tar xfz $SRC_ARCHIVE
+pushd *$LIBRARY*/
+./configure --prefix=${TMP_BUILD_DIR}
+make
+make install
+popd
+popd
+
+mkdir -p $OUTPUT_BLOBS_DIR
+
+pushd $TMP_BUILD_DIR
+tar cfz $OUTPUT_BLOBS_DIR/$LIBRARY-compiled-${VERSION}.tgz .
+popd
+
+sha=$(sha256sum $OUTPUT_BLOBS_DIR/$LIBRARY-compiled-${VERSION}.tgz | awk '{print $1}')
+
+git clone $ROOT_DIR $REPO_OUT
+cat > $REPO_OUT/manifest.yml <<YAML
+---
+language: $LIBRARY
+default_versions:
+- name: $LIBRARY
+  version: $VERSION
+dependency_deprecation_dates: []
+
+include_files:
+  - README.md
+  - VERSION
+  - bin/supply
+  - manifest.yml
+pre_package: scripts/build.sh
+
+dependencies:
+- name: $LIBRARY
+  version: $VERSION
+  uri: https://${S3_BUCKET}.s3.${S3_REGION}.amazonaws.com/blobs/$LIBRARY/$LIBRARY-compiled-$VERSION.tgz
+  sha256: ${sha}
+  cf_stacks:
+  - cflinuxfs3
+YAML
+
+# GIT!
+if [[ -z $(git config --global user.email) ]]; then
+  git config --global user.email "${GIT_EMAIL}"
+fi
+if [[ -z $(git config --global user.name) ]]; then
+  git config --global user.name "${GIT_NAME}"
+fi
+
+(cd ${REPO_OUT}
+ echo "${VERSION}" > VERSION
+ git merge --no-edit ${REPO_BRANCH}
+ git add -A
+ git status
+ git commit -m "bump ${LIBRARY} v${VERSION}")
